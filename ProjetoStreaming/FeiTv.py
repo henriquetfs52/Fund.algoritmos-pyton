ec864e25778dda838bedf391b88c5fb2967d195c
Henrique <henriquetfs52@gmail.com>
2026-04-17 15:47:52 -0300

    cadastro e login concluido

--- ProjetoStreaming/FeiTv.py
+++ ProjetoStreaming/FeiTv.py
@@ -1,6 +1,45 @@
-Inicio = int(input("Deseja se cadastrar, ou logar no serviço da FeiTv? Digite 0 para cadastrar e 1 para logar"))
+
+def menu():
+       while True: 
+        global Inicio
+        print("FEITV")
+        print("0 - Cadastrar")
+        print("1 - Logar")
+        print("2 - Sair")
+
+        Inicio = int(input("Digite sua resposta "))
+
+        if Inicio == 0:
+            askCad()
+            appendCad()
+            Aplicativo = True
+
+
+        elif Inicio == 1:
+        
+                askLog()
+                appendLog()
+                if Login == Cad:
+                    print(f"Olá {Usuário}, você fez o login na sua conta corretamente")
+                    Aplicativo = True
+                else:
+                    print("Você fez o login na sua conta incorretamente")
+                    Login.pop()
+                    Login.pop()
+                    Login.pop()
+
+        elif Inicio == 2:
+            break
+
+        else:
+            print ("O numero que digitou não é válido")
+        
+
+
 Login = []
 Cad = []
+Catalogo = ["FilmeA","FilmeA"]
+Favoritos = []
 Aplicativo = False
 def appendCad():
     Cad.append(Usuário)
@@ -8,39 +47,33 @@ def appendCad():
     Cad.append(Senha)
 
 def appendLog():
-    Login.append(Usuário)
-    Login.append(Email)
-    Login.append(Senha)
+    Login.append(UsuárioL)
+    Login.append(EmailL)
+    Login.append(SenhaL)
+
+        
 
 def askCad():
-    Usuário = input("Digite o seu novo usuário")
-    Email = input("Digite o seu email")
-    Senha = input("Digite a sua nova senha")
+    global Usuário
+    global Email
+    global Senha
+
+    Usuário = str(input("Digite o seu novo usuário: "))
+    Email = str(input("Digite o seu email: "))
+    Senha = str(input("Digite a sua nova senha: "))
 
 def askLog():
-    UsuárioL = input("Digite o seu usuário")
-    EmailL = input("Digite o seu email")
-    SenhaL = input("Digite a sua senha")
+    global UsuárioL
+    global EmailL
+    global SenhaL
 
-if Inicio == 0:
-    askCad()
-    appendCad()
-    Aplicativo = True
+    UsuárioL = str(input("Digite o seu usuário: "))
+    EmailL = str(input("Digite o seu email: "))
+    SenhaL = str(input("Digite a sua senha: "))
 
 
-elif Inicio == 1:
-    while Aplicativo == False:
-        askLog()
-        appendLog()
-        if Login == Cad:
-            print("Você fez o login na sua conta corretamente")
-            Aplicativo = True
-        else:
-            print("Você fez o login na sua conta incorretamente")
 
-else:
-    print ("O numero que digitou não é válido")
-
+menu()
 
 
 
